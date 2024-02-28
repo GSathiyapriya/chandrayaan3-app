87f78dd6cb9f8fc5fbf7559b336d2fc4ae892331
Sathiyapriya <gsathiyapriyaganesan@gmail.com>
2024-02-28 18:21:27 +0530

    Added spacecraft services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SpacecraftService } from './spacecraft/spacecraft.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SpacecraftService],
 })
 export class AppModule {}
